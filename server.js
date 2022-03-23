edddb10023dfe968fb9f86dfb2dc00f920314072
Albert <sab4856@gmail.com>
2022-03-24 01:44:24 +0800

    feat: add Cors config

--- server.js
+++ server.js
@@ -2,25 +2,33 @@ const http = require('http');
 
 const requestListener = (req, res) => {
 	const headers = {
-		'Content-Type': 'text/plain',
+		'Access-Control-Allow-Headers':
+			'Content-Type, Authorization, Content-Length, X-Requested-With',
+		'Access-Control-Allow-Origin': '*',
+		'Access-Control-Allow-Methods': 'PATCH, POST, GET,OPTIONS,DELETE',
+		'Content-Type': 'application/json',
 	};
 	const splitRequest = (url, method = 'GET') =>
 		req.url === url && req.method === method;
 
 	if (splitRequest('/')) {
 		res.writeHead(200, headers);
-		res.write('Index');
-		res.end();
-		return;
-	}
-	if (splitRequest('/', 'DELETE')) {
-		res.writeHead(200, headers);
-		res.write('Deleted.');
+		res.write(
+			JSON.stringify({
+				status: 'SUCCESS',
+				data: [],
+			})
+		);
 		res.end();
 		return;
 	}
 	res.writeHead(404, headers);
-	res.write('Not Found.');
+	res.write(
+		JSON.stringify({
+			status: 'ERROR',
+			message: '查無此網站路由',
+		})
+	);
 	res.end();
 };
 
