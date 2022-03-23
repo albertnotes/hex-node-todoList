6ec4b9466ad85e7246307f2f3081619b655776d1
Albert <sab4856@gmail.com>
2022-03-24 03:09:11 +0800

    feat: 建立 data chunk - 接收 body 資料(模擬)

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
 const http = require('http');
+const { v4: uuidv4 } = require('uuid');
+const todos = [
+	{
+		id: uuidv4(),
+		title: '今天要刷牙',
+	},
+];
 
 const requestListener = (req, res) => {
 	const headers = {
@@ -11,26 +18,47 @@ const requestListener = (req, res) => {
 	const splitRequest = (url, method = 'GET') =>
 		req.url === url && req.method === method;
 
-	if (splitRequest('/')) {
+	let body = '';
+	req.on('data', chunk => {
+		console.log(chunk);
+		body += chunk;
+	});
+	req.on('end', () => {
+		console.log('data', body);
+	});
+
+	if (req.method === 'OPTIONS') {
+		res.writeHead(200, headers);
+		res.end();
+	} else if (splitRequest('/todos')) {
 		res.writeHead(200, headers);
 		res.write(
 			JSON.stringify({
 				status: 'SUCCESS',
-				data: [],
+				data: todos,
 			})
 		);
 		res.end();
-		return;
+	} else if (splitRequest('/todos', 'POST')) {
+		res.writeHead(201, headers);
+		res.write(
+			JSON.stringify({
+				status: 'SUCCESS',
+				data: todos,
+			})
+		);
+		res.end();
+	} else {
+		res.writeHead(404, headers);
+		res.write(
+			JSON.stringify({
+				status: 'ERROR',
+				message: '查無此網站路由',
+			})
+		);
+		res.end();
 	}
-	res.writeHead(404, headers);
-	res.write(
-		JSON.stringify({
-			status: 'ERROR',
-			message: '查無此網站路由',
-		})
-	);
-	res.end();
 };
 
 const server = http.createServer(requestListener);
-server.listen(8000);
+server.listen(3005);
